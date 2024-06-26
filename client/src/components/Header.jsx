897a8938948d915a2a4fc8120a77d5dbc7cd13ca
PryCoder <priyanshugupta007007@gmail.com>
2024-06-26 22:58:08 +0530

    complete create post functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,131 +1,117 @@
-import { useState } from 'react'; // Import useState hook
+import { useState } from 'react';
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { Link , useLocation} from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon, FaSun } from 'react-icons/fa';
-import {useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { toggleTheme } from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
 
-
-
-
 export default function Header() {
-const path = useLocation().pathname;
- const dispatch = useDispatch();
-  const {currentUser} = useSelector(state => state.user)
-  const {theme} = useSelector(state => state.theme);
-const handleSignout = async () => {
-try {
-const res = await fetch('/api/user/signout',{
-method:"POST",
-});
-const data = await res.json();
-if (!res.ok) {
-console.log(data.message);
-} else {
- dispatch(signoutSuccess());
-}
-} catch(error) {
-console.log(error.message);
-}
+  const path = useLocation().pathname;
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(state => state.user);
+  const { theme } = useSelector(state => state.theme);
 
-}
+  const handleSignout = async () => {
+    try {
+      const res = await fetch('/api/user/signout', {
+        method: "POST",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   // State to manage the visibility of dropdown menu
   const [showDropdown, setShowDropdown] = useState(false);
 
   return (
-    <Navbar className="bg-white px-2 py-2.5 dark:border-gray-700 dark:bg-gray-800 sm:px-4 border-b-2 flex justify-between items-center ">
+    <Navbar className="bg-white px-2 py-2.5 dark:border-gray-700 dark:bg-gray-800 sm:px-4 border-b-2 flex justify-between items-center">
       <Link to="/" className="text-sm sm:text-xl font-semibold dark:text-white">
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Epic</span>
         Shot
       </Link>
       <div className="hidden lg:inline dark:border-gray-700">
-        <form className="relative ">
-          <TextInput 
+        <form className="relative">
+          <TextInput
             type="text"
             placeholder="Search..."
-            inputclassName="pl-10 " // Adjust input padding to accommodate the icon
+            className="pl-10" // Adjust input padding to accommodate the icon
           />
-          <AiOutlineSearch className="absolute top-1/2 transform -translate-y-1/2 right-3 " />
+          <AiOutlineSearch className="absolute top-1/2 transform -translate-y-1/2 right-3" />
         </form>
       </div>
-      <Button className="w-12 h-10 lg:hidden flex justify-center items-center rounded-xl  dark:bg-gray-700">
-  <AiOutlineSearch className="text-xl dark:text-white " /> {/* Increase the size of the icon */}
-</Button>
+      <Button className="w-12 h-10 lg:hidden flex justify-center items-center rounded-xl shadow-sm-lg bg-gray-100 dark:bg-gray-700">
+        <AiOutlineSearch className="text-xl dark:text-white text-black" /> {/* Increase the size of the icon */}
+      </Button>
 
-
-      <div className="hidden lg:inline ">
-        <Link to="/" className=" dark:text-white text-black mr-6">Home</Link>
-        <Link to="/about" className=" dark:text-white text-black mr-6">About</Link>
-        <Link to="/projects" className=" dark:text-white text-black">Projects</Link>
+      <div className="hidden lg:inline">
+        <Link to="/" className="dark:text-white text-black mr-6">Home</Link>
+        <Link to="/about" className="dark:text-white text-black mr-6">About</Link>
+        <Link to="/projects" className="dark:text-white text-black">Projects</Link>
       </div>
- 
-      <Button className="w-12 h-10 hidden sm:inline dark:bg-gray-700 dark:border-gray-700  shadow -lg dark:shadow-lg" color="gray"  onClick={() => dispatch(toggleTheme())} >
-       <div className="flex justify-center items-center w-full h-full" >
-       {theme === 'light' ? <FaSun className="text-xl " /> : <FaMoon className="text-xl" />}
-         {/* Increase the size of the icon */}
+
+      <Button className="w-12 h-10 hidden sm:inline dark:bg-gray-700 dark:border-gray-700 shadow-lg dark:shadow-lg" color="gray" onClick={() => dispatch(toggleTheme())}>
+        <div className="flex justify-center items-center w-full h-full">
+          {theme === 'light' ? <FaSun className="text-xl text-orange-500" /> : <FaMoon className="text-xl dark:text-white" />}
         </div>
       </Button>
-   {currentUser ? (
 
-    <Dropdown
-    arrowIcon={false}
-    inline
-    label={
-      <Avatar
-        alt='user'
-        img={currentUser.profilePicture}
-        className="rounded-full overflow-hidden w-10 h-10" // Ensure size classes are added
-      />
-    }
-  >
-    <Dropdown.Header>
-      <span className="block text-sm">@{currentUser.username}</span>
-      <span className="block text-sm font-medium truncate">{currentUser.email}</span>
-    </Dropdown.Header>
-    <Link to={'/dashboard?tab=profile'}>
-      <Dropdown.Item>Profile</Dropdown.Item>
-    </Link>
-    <Dropdown.Divider />
-    <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
-  </Dropdown>
-
-   ) :
-  (
-    <Link to='/sign-in'>
-   <button type="button" class="group flex items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none text-white bg-gradient-to-br from-purple-600 to-cyan-500 enabled:hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800 border-0 rounded-lg focus:ring-2"><span class="items-center flex justify-center bg-white text-gray-900 transition-all duration-75 ease-in group-enabled:group-hover:bg-opacity-0 group-enabled:group-hover:text-inherit dark:bg-gray-900 dark:text-white w-full rounded-md text-sm px-4 py-2 border border-transparent">Sign in</span></button>
-   </Link>
-  )
-  
-  }
-      {/* Sign In button */}
-      
+      {currentUser ? (
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt='user'
+              img={currentUser.profilePicture}
+              className="rounded-full overflow-hidden w-10 h-10" // Ensure size classes are added
+            />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">@{currentUser.username}</span>
+            <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+          </Dropdown.Header>
+          <Link to={'/dashboard?tab=profile'}>
+            <Dropdown.Item>Profile</Dropdown.Item>
+          </Link>
+          <Dropdown.Divider />
+          <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
+        </Dropdown>
+      ) : (
+        <Link to='/sign-in'>
+          <button type="button" className="group flex items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none text-white bg-gradient-to-br from-purple-600 to-cyan-500 enabled:hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800 border-0 rounded-lg focus:ring-2">
+            <span className="items-center flex justify-center bg-white text-gray-900 transition-all duration-75 ease-in group-enabled:group-hover:bg-opacity-0 group-enabled:group-hover:text-inherit dark:bg-gray-900 dark:text-white w-full rounded-md text-sm px-4 py-2 border border-transparent">Sign in</span>
+          </button>
+        </Link>
+      )}
 
       {/* Dropdown toggle button */}
- <div className="relative lg:hidden">
-  <Button
-    className="text-black dark:text-white bg-gray-100 shadow-lg dark:bg-gray-700"
-    onClick={() => setShowDropdown(!showDropdown)}
-  >
-    <AiOutlineMenu className="text-xl" />
-  </Button>
+      <div className="relative lg:hidden">
+        <Button
+          className="text-black dark:text-white bg-gray-100 shadow-lg dark:bg-gray-700"
+          onClick={() => setShowDropdown(!showDropdown)}
+        >
+          <AiOutlineMenu className="text-xl" />
+        </Button>
 
         {/* Dropdown content */}
         {showDropdown && (
-          <div className="absolute bg-white dark:bg-gray-700 top-full left-0 mt-1 w-40 py-2 rounded-lg shadow-lg">
-            <Link to="/" className="block px-4 py-2 
-             dark:text-white dark:bg-gray-700 text-gray-800 dark:hover:bg-gray-900 hover:bg-gray-200">Home</Link>
-            <Link to="/about" className="block px-4 py-2 
-             dark:text-white dark:bg-gray-700 text-gray-800 dark:hover:bg-gray-900 hover:bg-gray-200">About</Link>
-            <Link to="/projects" className="block px-4 py-2 
-             dark:text-white dark:bg-gray-700 text-gray-800 dark:hover:bg-gray-900 hover:bg-gray-200">Projects</Link>
+          <div className="absolute right-0 mt-2 w-40 rounded-lg shadow-lg bg-white dark:bg-gray-700">
+            <Link to="/" className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-900">Home</Link>
+            <Link to="/about" className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-900">About</Link>
+            <Link to="/projects" className="block px-4 py-2 text-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-900">Projects</Link>
           </div>
         )}
       </div>
-
-      {/* Responsive Dropdown content */}
-    
     </Navbar>
   );
 }
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,12 +6,15 @@ import 'react-quill/dist/quill.snow.css';
 import { app } from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function CreatePost() {
     const [file, setFile] = useState(null);
     const [imageUploadProgress, setImageFileUploadProgress] = useState(null);
     const [imageUploadError, setImageFileUploadError] = useState(null);
     const [formData, setFormData] = useState({});
+    const [publishError, setPublishError] = useState(null);
+    const navigate = useNavigate();
 
     const handleUploadImage = async () => {
         try {
@@ -50,15 +53,49 @@ export default function CreatePost() {
         }
     };
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('/api/post/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if (!res.ok) {
+                setPublishError(data.message);
+                return;
+            }
+         
+            if (res.ok) {
+                setPublishError(null);
+                navigate(`/post/${data.slug}`)
+            }
+        } catch (error) {
+            setPublishError('Something went wrong');
+        }
+    };
+
     return (
         <div className='p-3 max-w-3xl mx-auto min-h-screen'>
             <h1 className='text-center text-3xl my-7 font-semibold'>
                 Create a post
             </h1>
-            <form className='flex flex-col gap-4'>
+            <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
                 <div className='flex flex-col gap-4 sm:flex-row justify-between'>
-                    <TextInput type='text' placeholder='Title' required id='title' className='flex-1' />
-                    <Select>
+                    <TextInput
+                        type='text'
+                        placeholder='Title'
+                        required
+                        id='title'
+                        className='flex-1'
+                        onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+                    />
+                    <Select
+                        onChange={(e) => setFormData({ ...formData, category: e.target.value })}
+                    >
                         <option value="uncategorized">Select a category</option>
                         <option value="javascript">Javascript</option>
                         <option value="reactjs">React.js</option>
@@ -71,7 +108,7 @@ export default function CreatePost() {
                         type="button"
                         className="group flex items-center justify-center p-0.5 text-center font-medium relative focus:z-10 focus:outline-none text-white bg-gradient-to-br from-purple-600 to-cyan-500 enabled:hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800 border-0 rounded-lg focus:ring-2"
                         size='sm'
-                        outline
+                        outline="true"
                         onClick={handleUploadImage}
                         disabled={imageUploadProgress !== null}
                     >
@@ -87,21 +124,31 @@ export default function CreatePost() {
                     </button>
                 </div>
                 {imageUploadError && 
-                <Alert color="failure">{imageUploadError}</Alert>
+                    <Alert color="failure">{imageUploadError}</Alert>
                 }
                 {formData.image && (
-                <img
-                src={formData.image}
-                alt='upload'
-                className='w-full h-72 object-cover' />
+                    <img
+                        src={formData.image}
+                        alt='upload'
+                        className='w-full h-72 object-cover'
+                    />
                 )}
-                <ReactQuill theme='snow' placeholder='Write Something..' className='h-72 mb-12 dark:placeholder:text-white' required />
+                <ReactQuill
+                    theme='snow'
+                    placeholder='Write Something..'
+                    className='h-72 mb-12 dark:placeholder:text-white'
+                    required
+                    onChange={(value) => setFormData({ ...formData, content: value })}
+                />
                 <Button
                     type="submit"
                     className="w-full bg-gradient-to-r from-purple-600 to-pink-600 text-white py-1 px-2 rounded-lg text-sm md:text-base hover:bg-gradient-to-r hover:from-purple-700 hover:to-pink-700"
                 >
                     Publish
                 </Button>
+                {
+                publishError && <Alert className='mt-5' color={failure}>{publishError}</Alert>
+                }
             </form>
         </div>
     );
